8a7d48454fe401382f05dcc6aeb7f6e4299386e8
3268519586@qq.com <felix@felix>
2024-03-20 20:24:24 +0800

    on 33. many changes. forgotten.

--- backtest.cpp
+++ backtest.cpp
@@ -5,11 +5,30 @@ struct DataFrame {
 };
 
 struct Backtest {
-
+	std::vector<std::string> symbols;
+	using unit_type = long long;
+	std::vector<unit_type> units;
+	using amount_type = long double;
+	std::vector<amount_type> positions;
+	amount_type amount;
+	using rate_type = long double;
+	rate_type maker_ftc;
+	rate_type maker_ptc;
+	rate_type taker_ftc;
+	rate_type taker_ptc;
+	using seq_type = int;
+	seq_type trade_seq_num;
+	Backtest() = default;
+	void buy();
+	void sell();
+	void run(std::string start_date, std::string end_date);
 };
 
-auto main() -> int {
-	string data_path = "TSLA.csv";
-	return 0;
+void gridtrading() {
+
 }
 
+auto main() -> int {
+	std::string data_path = "TSLA.csv";
+	return 0;
+}
